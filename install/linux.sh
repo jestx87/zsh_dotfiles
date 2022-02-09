4a40cbdb995df724103ec888520f7a74527500c2
Jens Fitzenetz <jens.fitzenetz@pharma-mall.de>
2022-02-09 07:42:01 +0100

    added folder install with linux.sh

---
+++ install/linux.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+sh -c "$(curl -fsSL https://starship.rs/install.sh)"
+ln -s ~/.dotfiles/.config .config
+ln -s ~/.dotfiles/.vimrc .vimrc
+echo "install fzf manually"
